cac872134c7e3bdd486822cae3bbeb1db3b004fd
boghdady <boghdady107@gmail.com>
2020-10-28 01:55:44 +0200

    Analyze How many times man united team win

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ const matches: string[][] = fs.readFileSync('football.csv',{
 .split('\n')
 .map((row: string): string[] => row.split(',') );
 
-console.log(matches);
+
+let manUnitedWin = 0;
+
+for(let match of matches) {
+  if(match[1] === "Man United" && match[5]=== 'H') {
+    manUnitedWin++;
+  } else if (match[2] === "Man United" && match[5]=== 'A') {
+    manUnitedWin++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWin} games`);
 
 
